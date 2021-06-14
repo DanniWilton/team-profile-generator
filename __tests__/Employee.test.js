8368268e5df991d4b6b10aab19d0f7212136b9be
Danni Wilton <danniwilton@gmail.com>
2021-06-14 22:43:46 +0800

    first commit

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,42 @@
+// manager, engineer, intern
+
+// manager questions
+// name
+// email
+// id
+// team/ office number
+
+// engineer
+// name
+// email
+// github
+// id
+// team/ office number
+
+// intern 
+// name
+// email
+// school
+// id
+// team/office number
+
+const { isMainThread } = require("node:worker_threads");
+const Employee = require("../employee.js.js");
+
+describe('employee', () => {
+    it("throws an error")
+})
+
+//name
+
+// id
+
+// email
+
+// getName()
+
+// getId()
+
+// getEmail()
+
+// getRole()—returns 'Employee'
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,46 @@
+// manager, engineer, intern
+
+// manager questions
+// name
+// email
+// id
+// team/ office number
+
+// engineer
+// name
+// email
+// github
+// id
+// team/ office number
+
+// intern 
+// name
+// email
+// school
+// id
+// team/office number
+
+const { isMainThread } = require("node:worker_threads");
+const Employee = require("../employee.js.js");
+
+describe('employee', () => {
+    it("throws an error")
+})
+
+// name
+
+// id
+
+// email
+
+// getName()
+
+// getId()
+
+// getEmail()
+
+// getRole()—returns 'Employee'
+
+// officeNumber
+
+// getRole()—overridden to return 'Manager'
