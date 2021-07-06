992a2074ba772866878130d0a988be6a3122ce58
Danni Wilton <danniwilton@gmail.com>
2021-07-06 12:12:35 +0800

    added gitignore, worked on index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const employee = require("./lib/employee.js");
+// const employee = require("./lib/employee.js");
 const manager = require("./lib/manager.js");
 const intern = require("./lib/intern.js");
-const engineer = require("./lib/engineer.js")
+const engineer = require("./lib/engineer.js");
+let currentTeam = ``;
 
 
 inquirer.prompt([
             {
-                type: input,     
+                type: "input",     
                 name: 'managerName',
                 message: 'Enter Manager name: '
             },
             {
-                type: input,
+                type: "input",
                 name: 'managerEmail',
                 message: 'Enter Manager email address: ',
             },
             {
-                type: input,
+                type: "input",
                 name: 'managerId',
                 message: 'Enter Manager ID: ',
             },
             {
-                type: input,
+                type: "input",
                 name: 'managerOfficeNumber',
                 message: 'Enter manager office number: ',
             },
@@ -56,22 +57,22 @@ inquirer.prompt([
 function engineerQuestions() {
     inquirer.prompt([
         {
-            type: input,
+            type: "input",
             name: 'name',
             message: 'Enter Engineer name: ',
         },
         {
-            type: input,
+            type: "input",
             name: 'email',
             message: 'Enter Engineer email address: ',
         },
         {
-            type: input,
+            type: "input",
             name: 'id',
             message: 'Enter Engineer ID: ',
         },
         {
-            type: input,
+            type: "input",
             name: 'github',
             message: 'Enter Engineer github username: ',
         },
@@ -85,7 +86,7 @@ function engineerQuestions() {
             );
             let employeeCardHtml = addTeamMemberToCard('engineer', teamMember);
             currentTeam = currentTeam + employeeCardHtml;
-            if (answers.jobRoles = "Engineer") {
+            if (answers.jobRoles = "engineer") {
                 engineerQuestions();
             } else if (answers.jobRoles = "Intern") {
                 internQuestions();
@@ -96,22 +97,22 @@ function engineerQuestions() {
 function internQuestions() {
     inquirer.prompt([
         {
-            type: input,
+            type: "input",
             name: 'name',
             message: 'Enter Intern name: ',
         },
         {
-            type: input,
+            type: "input",
             name: 'email',
             message: 'Enter Intern email address',
         },
         {
-            type: input,
+            type: "input",
             name: 'id',
             message: 'Enter Intern ID: ',
         },
         {
-            type: input,
+            type: "input",
             name: 'school',
             message: 'Enter Intern\s school: ',
         }
@@ -123,15 +124,65 @@ function internQuestions() {
                 answers.id,
                 answers.school
             );
-            let employeeCardHtml = addTeamMemberToCard('manager', teamMember);
+            let employeeCardHtml = addTeamMemberToCard('intern', teamMember);
             currentTeam = currentTeam + employeeCardHtml;
-            if (answers.jobRoles = "Engineer") {
+            if (answers.jobRoles = "engineer") {
                 engineerQuestions();
-            } else if (answers.jobRoles = "Intern") {
+            } else if (answers.jobRoles = "intern") {
                 internQuestions();
             } else {
                 writeToFile();
             };
             })})};
 
+
+            function writeToFile() {
+                console.log("added team is " + currentTeam);
+                fs.writeFile("./dist/index.html", html(currentTeam), (error) =>
+                error ? console.log(error): console.log("Completed!"));
+            }
+            function addTeamMemberToCard(jobRoles, data) {
+                if(jobRoles == "manager"){
+                    return `
+                    <div>
+                    <p>Name: ${data.managerName}<p>
+                    <p>ID: ${data.managerId}<p>
+                    <p><a href="mailto:${data.managerEmail}">Email: $data.managerEmail}</a></p>
+                    <p>Office Number: ${data.managerOfficeNumber}</p>
+                    </div>`;
+                } else if (jobRoles == "engineer"){
+                    return`
+                    <div>
+                    <p>Name: ${data.name}<p>
+                    <p>ID: ${data.id}<p>
+                    <p><a href="mailto:${data.email}">Email: $data.email}</a></p>
+                    <p><a href="${data.github}" target="_blank">Github</a></p>
+                    </div>
+                    `;
+                } else {
+                    return `
+                    <div>
+                    <p>Name: ${data.name}<p>
+                    <p>ID: ${data.id}<p>
+                    <p><a href="mailto:${data.email}">Email: $data.email}</a></p>
+                    <p>School: ${data.school}</p>
+                    </div>
+                    `;
+                }
+            }
+
+            function html(currentTeam) {
+                return `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                    
+                </body>
+                </html>`;
+            }
 
